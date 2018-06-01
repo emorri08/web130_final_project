b169ea1504e7b5ce7194f70c0c5da94a2685db88
David Holter <djholter@gmail.com>
2018-06-01 04:44:11 +0000

    replaced code with Josh's

--- js/graphql/articles.js
+++ js/graphql/articles.js
@@ -1,21 +1,31 @@
-let getAllArticles = '
-    query AllArticle {
-        allArticles {
-            id, 
-            title,
-            content
-        }
-    }
-';
+/* global $ */
 
-$(document).ready(function()) {
-    $.post(
-        'https://api.graph.cool/simple/v1/cjhjt273h019p0170q9p730ti',
-        JSon.stringify({
-             query: getAllArticles
+let getAllArticles = `
+    query AllArticles {
+      allArticles {
+        id,
+        title,
+        content
+      }
+    }
+`;
+
+$(document).ready(function() {
+    $.post({
+        url: 'https://api.graph.cool/simple/v1/cjhjspp3l43x40186ohece9if',
+        data: JSON.stringify({
+            query: getAllArticles
         }),
-        (response) => {
-            console.log(response);
-        }
-    );
+        success: (response) => {
+            let articles = response.data.allArticles;
+            console.log(articles);
+            let html = '';
+            for (let article of articles) {
+                html += `<h2>${article.title}</h2>
+                         <p>${article.content}</p>`;
+            }
+            $('#main-content').html(html);
+        },
+        contentType: 'application/json'
+    });
 });
